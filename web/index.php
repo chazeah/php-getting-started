1939b0c33a6d895c81757025c3c7fdb41dd7b480
Charley Walton <charley.walton@thetradedesk.com>
2022-01-16 10:48:46 -0700

    adding db route

--- web/index.php
+++ web/index.php
@@ -12,7 +12,20 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+  'twig.path' => __DIR__.'/views',
+));
+
+// Register the DB
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'), array(
+  'pdo.server' => array(
+      'driver'   => 'pgsql',
+      'user' => $dbopts["user"],
+      'password' => $dbopts["pass"],
+      'host' => $dbopts["host"],
+      'port' => $dbopts["port"],
+      'dbname' => ltrim($dbopts["path"],'/')
+    )
 ));
 
 // Our web handlers
@@ -30,6 +43,21 @@ $app->get('/cowsay', function() use($app) {
 $app->get('/hello', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return str_repeat('Hello', getenv('TIMES'));
+});
+
+$app->get('/db', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
 });
 
 $app->run();
